f81aa48886dc9c6aa582737f42203ded8d0a038a
Michael <michael@pixt.us>
2019-09-15 20:26:16 +0700

    set up for animation

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,8 @@ import withTracker from "./lib/withTracker";
 import ScrollToTopRoute from "./lib/ScrollToTopRoute";
 import Home from "./containers/Home";
 import Navigation from "./components/Navigation";
+import Content from "./components/Content";
+import Webinar from "./components/Webinar";
 import Contact from "./components/Contact";
 
 class Routes extends Component {
@@ -14,6 +16,8 @@ class Routes extends Component {
                     <Navigation />
                     <Switch>
                         <ScrollToTopRoute path="/contact" component={withTracker(Contact)} />
+                        <ScrollToTopRoute path="/webinar" component={withTracker(Webinar)} />
+                        <ScrollToTopRoute path="/about" component={withTracker(Content)} />
                         <ScrollToTopRoute path="/*" component={withTracker(Home)} />
                     </Switch>
                 </main>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Button } from "./presentation/Button";
 
+import logo from "../images/MCA.svg"
+
 class Navigation extends Component {
     render() {
         return (
@@ -9,7 +11,7 @@ class Navigation extends Component {
 					<nav className="uk-navbar uk-navbar-container uk-navbar-transparent" data-uk-navbar>
 						<div className="uk-navbar-left">
 							<div className="uk-navbar-item uk-padding-remove-horizontal">
-								<p style={{fontWeight: "200 !important"}} className="uk-h1">MC+A</p>
+                                <img src={logo} width="150px" alt="Phoenix Logo" data-uk-image />
 							</div>
 						</div>
 						<div className="uk-navbar-right" data-uk-scrollspy="cls: uk-animation-fade; offset-top: 400; delay: 100; hidden: true;">
